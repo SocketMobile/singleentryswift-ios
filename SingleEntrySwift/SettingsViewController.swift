b473b80f63bfc84985445f7643fac36e034643b7
Eric Glaenzer <ericg@socketmobile.com>
2015-10-28 15:21:52 -0400

    add the settings screen with SoftScan enable button

---
+++ SingleEntrySwift/SettingsViewController.swift
@@ -0,0 +1,100 @@
+//
+//  SettingsViewController.swift
+//  SingleEntrySwift
+//
+//  Created by Eric Glaenzer on 10/27/15.
+//  Copyright © 2015 Socket Mobile, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, ScanApiHelperDelegate{
+    var detailItem: AnyObject?
+
+    @IBOutlet weak var softscan: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        ScanApiHelper.sharedScanApiHelper().pushDelegate(self)
+        // Do any additional setup after loading the view.
+        
+        // retrieve the current status of SoftScan
+        ScanApiHelper.sharedScanApiHelper().postGetSoftScanStatus(self, response: "onGetSoftScanStatus:")
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        ScanApiHelper.sharedScanApiHelper().popDelegate(self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func changeSoftScan(sender: AnyObject) {
+        if(!softscan.on){
+            print("disabling SoftScan...")
+            ScanApiHelper.sharedScanApiHelper().postSetSoftScanStatus(UInt8(kSktScanDisableSoftScan), target: self, response: "onSetSoftScanStatus:")
+        }
+        else{
+            print("enabling SoftScan...")
+            ScanApiHelper.sharedScanApiHelper().postSetSoftScanStatus(UInt8(kSktScanEnableSoftScan), target: self, response: "onSetSoftScanStatus:")
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - ScanApiHelper results
+    func onGetSoftScanStatus(scanObj: ISktScanObject!) {
+        print("onGetSoftScanStatus received!")
+        let result = scanObj.Msg().Result()
+        print("Result:", result)
+        if(result == ESKT_NOERROR){
+            let status = Int(scanObj.Property().getByte())
+            print("receive SoftScan status:",status)
+            if ( status == kSktScanEnableSoftScan){
+                softscan.on = true
+            }
+            else{
+                softscan.on = false
+                if(status == kSktScanSoftScanNotSupported){
+                    ScanApiHelper.sharedScanApiHelper().postSetSoftScanStatus(UInt8(kSktScanSoftScanSupported), target: self, response: "onSetSoftScanStatus:")
+                }
+            }
+        }
+    }
+    
+    func onSetSoftScanStatus(scanObj: ISktScanObject){
+        
+    }
+    
+    // MARK: - ScanApiHelper Delegates
+    /**
+    * called each time a device connects to the host
+    * @param result contains the result of the connection
+    * @param newDevice contains the device information
+    */
+    func onDeviceArrival(result: SKTRESULT, device deviceInfo: DeviceInfo!) {
+        print("Settings: Device Arrival")
+    }
+    
+    /**
+    * called each time a device disconnect from the host
+    * @param deviceRemoved contains the device information
+    */
+    func onDeviceRemoval(deviceRemoved: DeviceInfo!) {
+        print("Settings: Device Removal")
+    }
+    
+
+}
