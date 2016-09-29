a58baabcae20cd611717f541edb9aa449a6d2b28
Eric Glaenzer <ericg@socketmobile.com>
2016-09-28 20:29:18 -0400

    modify the ScanApiHelper.swift header

--- SingleEntrySwift/ScanApiHelperExtension.swift
+++ SingleEntrySwift/ScanApiHelperExtension.swift
@@ -10,6 +10,17 @@
 //  Created by Eric Glaenzer on 9/22/16.
 //  Copyright © 2016 Socket Mobile, Inc. All rights reserved.
 //
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 import Foundation
 
@@ -24,8 +35,8 @@ extension ScanApiHelper
         scanObj?.property().setUlong(SKTDATACONFIRMATION(0, UInt8(kSktScanDataConfirmationRumbleGood), UInt8(kSktScanDataConfirmationBeepGood), UInt8(kSktScanDataConfirmationLedGreen)))
         let getCommand = false // this is a Set property command
         let command = CommandContext(param: getCommand, scanObj: scanObj, scanDevice: deviceHandle, device: device, target: target, response: response)
-        
+
         addCommand(command)
-        
+
     }
 }
